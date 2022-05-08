3ead71a15472e9cee983afb0207ebad386832e30
Aditya <aditya.gajbhiye.20@gmail.com>
2022-05-08 19:37:05 +0530

    added tailwind

--- packages/ui/src/main.tsx
+++ packages/ui/src/main.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
-import App from './App';
 import Login from '@components/login';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
