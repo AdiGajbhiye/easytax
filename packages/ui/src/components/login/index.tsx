488028aa4ab642fde73a59864b6555c973019060
Aditya <aditya.gajbhiye.20@gmail.com>
2022-05-08 23:14:11 +0530

    added login styling

--- packages/ui/src/components/login/index.tsx
+++ packages/ui/src/components/login/index.tsx
@@ -16,18 +16,25 @@ function Login() {
   const onSubmit: SubmitHandler<LoginForm> = (data) => console.log(data);
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <TextField name="email" label="Email" formProps={register('email', { required: true })} error={errors?.email} />
-        <TextField
-          name="password"
-          label="Password"
-          formProps={register('password', { required: true, minLength: 8, maxLength: 20 })}
-          error={errors?.password}
-        />
-        <input type="submit" />
-      </form>
+    <div className="flex justify-center items-center h-screen">
+      <div className="flex flex-col items-center">
+        <div className="text-2xl">Login</div>
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-center">
+          <TextField
+            name="email"
+            label="Email"
+            formProps={register('email', { required: true })}
+            error={errors?.email}
+          />
+          <TextField
+            name="password"
+            label="Password"
+            formProps={register('password', { required: true, minLength: 8, maxLength: 20 })}
+            error={errors?.password}
+          />
+          <input type="submit" className="bg-blue-400 rounded py-2 mt-8 w-full" />
+        </form>
+      </div>
     </div>
   );
 }
